3d0fabd60efb682f3d312e521f60765e55be43c6
Sigmadsen <andrejmarockin@gmail.com>
2025-02-21 09:02:31 +0300

    Remove useless --env-file flag from run_tests.sh file

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -3,4 +3,4 @@
 docker compose exec web python manage.py migrate --noinput
 
 # Run tests in docker container
-docker compose exec --env-file .env web python manage.py test
+docker compose exec web python manage.py test
