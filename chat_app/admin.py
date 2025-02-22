15b0446207d5906ef99f15e74fb50dd77a3748bd
Sigmadsen <andrejmarockin@gmail.com>
2025-02-22 18:46:17 +0300

    Customize admin panel: show Thread and Message models

--- chat_app/admin.py
+++ chat_app/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from chat_app.models import Thread, Message
+
+
+@admin.register(Thread)
+class ThreadAdmin(admin.ModelAdmin):
+    list_display = ("id", "created", "updated", "get_participants")
+    list_filter = ("created", "updated")
+
+    def get_participants(self, obj):
+        return ", ".join([p.username for p in obj.participants.all()])
+
+    get_participants.short_description = "Participants"
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ("id", "sender", "text", "created", "is_read")
+    list_filter = ("is_read", "created")
+    search_fields = ("text", "sender__username")
